23f70b97c86302be964cb3033a44d9ce41bbcd4d
prudente-casa-pet <143100959+prudente-casa-pet@users.noreply.github.com>
2023-08-30 20:36:58 +0000

    teste calendario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +8,13 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, RouterLink],
+  
 })
 export class HomePage {
-  constructor() {}
+  meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+  hoje = new Date();          // Dia atual
+  mes = this.hoje.getMonth();      // Mês atual
+  ano = this.hoje.getFullYear();   // Ano atual
 }
