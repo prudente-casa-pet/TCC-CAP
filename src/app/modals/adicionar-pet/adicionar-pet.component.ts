953ab335bedda17313e308210056896e3e8785ff
prudente-casa-pet <143100959+prudente-casa-pet@users.noreply.github.com>
2023-09-07 22:40:54 +0000

    07/09/23 - Campos modal pet

--- src/app/modals/adicionar-pet/adicionar-pet.component.ts
+++ src/app/modals/adicionar-pet/adicionar-pet.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-adicionar-pet',
   templateUrl: './adicionar-pet.component.html',
   styleUrls: ['./adicionar-pet.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterLink]
 })
 export class AdicionarPetComponent  implements OnInit {
 
